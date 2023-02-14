07ead4c6839fd029fb524942ceeeed7a1f196ae6
aldyckybw <bagusaldycky@gmail.com>
2023-02-14 12:19:02 +0700

    fixed helper

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Http;
 
 function getUser($userId)
 {
-  $url = env('SERVICE_USER_URL') . 'users/' . $userId;
+  $url = env('SERVICE_USER_URL') . '/users/' . $userId;
 
   try {
     $response = Http::timeout(10)->get($url);
@@ -23,7 +23,7 @@ function getUser($userId)
 
 function getUserByIds($userIds = [])
 {
-  $url = env('SERVICE_USER_URL') . 'users/';
+  $url = env('SERVICE_USER_URL') . '/users/';
 
   try {
     if (count($userIds) === 0) {
@@ -49,7 +49,7 @@ function getUserByIds($userIds = [])
 
 function postOrder($params)
 {
-  $url = env('SERVICE_ORDER_PAYMENT_URL') . 'api/orders';
+  $url = env('SERVICE_ORDER_PAYMENT_URL') . '/api/orders';
   try {
     $response = Http::post($url, $params);
     $data = $response->json();
